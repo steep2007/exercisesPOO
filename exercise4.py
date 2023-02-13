b6acba301f7304632ef6f734ff13b419ef0896fc
stephani <steehin01@gmail.com>
2023-02-12 20:18:50 -0400

    First commit

--- exercise4.py
+++ exercise4.py
@@ -1,2 +1,19 @@
 # 4- Classe "Veículo": Crie uma classe chamada "Veículo" que tenha três atributos: "marca", "modelo" e "ano".
 # Adicione um método "mostrar_dados" que exibe esses dados.
+
+marca = input('')
+modelo = input('')
+ano = int(input(''))
+
+class Veiculo():
+
+    def __init__(self, marca, modelo, ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+
+    def mostrarDados(self):
+        print(f' Marca: {self.marca} \n Modelo: {self.modelo} \n Ano: {self.ano}')
+
+objeto = Veiculo(marca, modelo, ano)
+objeto.mostrarDados()
--- exercise5.py
+++ exercise5.py
@@ -1,3 +1,41 @@
 # 5- Classe "Conta Bancária": Crie uma classe chamada "ContaBancaria" que tenha três atributos:
 # "nome_titular", "numero_conta" e "saldo". Adicione métodos "depositar" e "sacar"
 # que permitam fazer depósitos e saques na conta. Adicione um método "mostrar_dados" que exibe esses dados.
+
+nome = input("Nome do titular: ")
+numero = int(input("Número da conta: "))
+saldo = float(input("Saldo inicial: "))
+acao = int(input("Quer sacar (1) ou depositar (0)? "))
+
+class ContaBancaria:
+    def __init__(self, nome, numero, saldo):
+        self.nomeTitular = nome
+        self.numeroConta = numero
+        self.saldo = saldo
+
+    def depositar(self, valor):
+        self.saldo = saldo + valor
+        print("Depósito realizado com sucesso!")
+
+    def sacar(self, valor):
+        if self.saldo >= valor:
+            self.saldo = saldo - valor
+            print("Saque realizado com sucesso!")
+        else:
+            print("Saldo insuficiente para saque.")
+
+    def mostrar_dados(self):
+        print(f"Titular: {self.nomeTitular}")
+        print(f"Número da conta: {self.numeroConta}")
+        print(f"Saldo: R${self.saldo:.2f}")
+
+conta = ContaBancaria(nome, numero, saldo)
+
+if acao == 0:
+    valor = float(input("Quantos você deseja depositar? "))
+    conta.depositar(valor)
+else:
+    valor = float(input("Quantos você deseja sacar? "))
+    conta.sacar(valor)
+
+conta.mostrar_dados()
